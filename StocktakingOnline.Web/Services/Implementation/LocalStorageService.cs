6526cfa57bd93196cdb5812372c51814eb140264
kaedei <kaedei@live.cn>
2018-05-21 15:06:10 +0800

    完成本地存储服务

--- StocktakingOnline.Web/Services/Implementation/LocalStorageService.cs
+++ StocktakingOnline.Web/Services/Implementation/LocalStorageService.cs
@@ -2,22 +2,40 @@
 using System.IO;
 using System.Threading.Tasks;
 using StocktakingOnline.Web.Services.Declaration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 namespace StocktakingOnline.Web.Services.Implementation
 {
 	public class LocalStorageService : IStorageService
 	{
-		public LocalStorageService()
+		private readonly IConfiguration configuration;
+		private readonly ILogger<LocalStorageService> logger;
+
+		public LocalStorageService(IConfiguration configuration, ILogger<LocalStorageService> logger)
 		{
+			this.configuration = configuration;
+			this.logger = logger;
 		}
 
 		public string GetFileDownloadUrl(string fileName)
 		{
-			throw new NotImplementedException();
+			var template = configuration["CloudStorage:Local:UrlTemplate"];
+			return template.Replace("{container}", configuration["CloudStorage:ImageContainer"])
+						   .Replace("{fileName}", fileName);
 		}
 
-		public Task UploadStreamToFile(string fileName, Stream stream)
+		public async Task UploadStreamToFile(string fileName, Stream stream)
 		{
-			throw new NotImplementedException();
+			if(string.IsNullOrWhiteSpace(fileName) || stream==null){
+				return;
+			}
+			var path = Path.Combine(configuration["CloudStorage:Local:Folder"],
+									configuration["CloudStorage:ImageContainer"],
+									fileName);
+			using(var fs = new FileStream(path, FileMode.Create))
+			{
+				await stream.CopyToAsync(fs);
+			}
 		}
 	}
 }
